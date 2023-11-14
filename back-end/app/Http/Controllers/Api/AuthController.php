5ee64b7e662d5372fe4585ebf5d2ee51a701bb3e
Viet Truong <viettruong0825@gmail.com>
2023-11-14 16:53:57 +0700

    done verify token

--- back-end/app/Http/Controllers/Api/AuthController.php
+++ back-end/app/Http/Controllers/Api/AuthController.php
@@ -15,6 +15,23 @@ use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
+    // Verify Token
+    public function verifyToken($token)
+    {
+        $user = User::where('verification_token', $token)->first();
+        if ($user) {
+            $user->email_verified_at = now();
+            $user->verification_token = null;
+            $user->save();
+
+            return response()->json([
+                'message' => 'Email verified successfully',
+                'user' => $user
+            ]);
+        }
+        return response()->json(['message' => 'Invalid verification token'], 422);
+    }
+    
     // Register account
     public function register(Request $request)
     {
